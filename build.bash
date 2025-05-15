72aef80566b58d8d835c5b94c6dc2172106ab7e8
3NikNikNik3 <->
2025-05-15 21:50:47 +0300

    Add release build mod

--- build.bash
+++ build.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-g++ -c generator.hpp generator.cpp
-g++ main.cpp generator.o -o SudokuGenerator
+if [ "$1" == "res" ]; then
+	g++ -c generator.hpp generator.cpp -O3 -s
+	g++ main.cpp generator.o -o SudokuGenerator -O3 -s
+else
+	g++ -c generator.hpp generator.cpp
+	g++ main.cpp generator.o -o SudokuGenerator
+fi
+
 rm generator.o generator.hpp.gch
